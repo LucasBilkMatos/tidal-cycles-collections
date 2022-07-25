ce6b28c3946470184214362bec6d814948f505a7
Daniel Dihardja <daniel@dihardja.de>
2022-07-25 18:20:25 +0200

    set with visuals

--- visuals/v-02.js
+++ visuals/v-02.js
@@ -1,11 +1,30 @@
-speed = 0.5
-shape(20,0.5,0.9)
-.color(0.9,0.9,50)
-  .scale(() => a.fft[1]*0.5)
-  .repeat(() => a.fft[2]*2)
-  .modulateRotate(o0, () => a.fft[0]*3)
-  .scale(() => a.fft[1]*0.5)
-  .modulate(noise(2,2))
-  .rotate(1, .2)
-  //.invert(2.4)
-.out(o0)
+
+
+hush()
+
+bpm = 140/4
+
+
+s0.initImage('http://pixapool.com/wp-content/uploads/2019/02/pixapool_QR8A1453.jpg')
+src(o0)
+  .layer(
+    src(s0)
+      .scale(() => 0.5 + a.fft[0]* 0.5)
+      // .scale(0.2)
+      .luma(.4)
+      .invert()
+      .kaleid([1, 2, 3, 7])
+      .rotate(.2, .1)
+      .contrast(2)
+      .scrollX(.1, -.02)
+      .scrollY(.5)
+    )
+    // .modulatePixelate(src(o0), [250, 500, 1000], [500, 2000])
+  .out(o0)
+
+
+a.show()
+
+a.hide()
+
+hush()
