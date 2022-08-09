107718dd69ad2b090044c92d37d04f5ffe3cfebb
Daniel Dihardja <daniel@dihardja.de>
2022-08-09 18:40:43 +0200

    hydra: update sketch

--- visuals/sketch.js
+++ visuals/sketch.js
@@ -1,65 +1,143 @@
+s0.initImage('http://localhost:8081/fish-01.png')
+src(o0)
+  .layer(src(o0).scale(.99))
+  .layer(src(s0).scale(()=> 0.8 + a.fft[0]*0.05).repeat(1,1))
+  .contrast(3)
+  .color(.3,.7,.6)
+  // .invert(.1)
+  .contrast(2.5)
+  .scale(1)
+  .kaleid(1)
+  .scrollX(.2, .01)
+  .out(o0)
+
+hush()
+
+
+
+s0.initImage('http://localhost:8081/fish-01.png')
+src(o0)
+  .layer(src(o0).scale(.99))
+  .layer(src(s0).scale(()=> .7 + a.fft[0]*0.02).repeat(2,2))
+  .contrast(3)
+  .color(.1,.8,.9)
+  // .invert(.1)
+  .contrast(2.5)
+  .scale(1)
+  // .rotate(.2,.1)
+  .kaleid(1)
+  .scrollX(.2, .01)
+  .out(o0)
+
+
+
+
+
+
+  s0.initImage('http://localhost:8081/fish-01.png')
+  src(o0)
+    .layer(src(o0).scale(.99))
+    .layer(src(s0).scale(()=> 0.3 + a.fft[0]*0.05).repeat(2,2))
+    .contrast(3)
+    .color(.2,.7,.1)
+    // .invert(.1)
+    .contrast(2.5)
+    .scale(1)
+    .kaleid(1)
+    .scrollX(.1, .01)
+    .out(o0)
+
+
 speed = 0.5
-shape(20,0.5,0.9)
-.color(100,100,100  )
-  .scale(() => a.fft[1]*0.5)
-  .repeat(() => a.fft[2])
-  .modulateRotate(o0, () => a.fft[0])
-  .scale(() => a.fft[1]*0.03)
-  .modulate(noise(2,2))
-  .rotate(1, .2)
-  //.invert(2.4)
+
+bpm =140
+
+
+    s0.initImage('http://localhost:8081/fish-01.png')
+    src(o0)
+      .layer(src(o0))
+      .layer(src(s0).scale(()=> 0.8 + a.fft[0]*0.05).repeat(2,2))
+      .contrast(3)
+      .color(.4,.7,.1)
+      // .invert(.1)
+      .contrast(2.5)
+      .scale(1)
+      .kaleid(1)
+      .scrollX(.05, .04)
+      .out(o0)
+
+
+
+
+
+      s0.initImage('http://localhost:8081/fish-01.png')
+      src(o0)
+        .layer(src(o0).scale(1.01).scrollX(.1, .05))
+        .layer(src(s0).scale(()=> 0.8 + a.fft[0]*0.05).repeat(2,2))
+        .contrast(3)
+        .color(.1,.1,.9)
+        // .invert(.1)
+        .contrast(2.5)
+        .scale(1)
+        .kaleid(2)
+        // .r()
+        // .scrollX(.1, .01)
+        .out(o0)
+
+bpm = 140
+
+speed = 2
+
+s0.initImage('http://localhost:8081/fish-01.png')
+src(o0)
+  .layer(src(o0).scale(1.01).rotate(.002))
+  .layer(src(s0).scale(()=> 0.5 + a.fft[0]*0.05))
+  .contrast(3)
+  .color(.3,.7,.4)
+  // .invert(.1)
+  .contrast(2.5)
+  .scale(1)
+  // .kaleid(3)
+  // .scrollX(.1, .01)
+  // .modulatePixelate(src(o0), 10, 10)
+  .out(o0)
+
+
+hush()
+
+a.hide()
+
+s0.initImage('http://localhost:8081/fish-01.png')
+src(o0)
+  .layer(src(o0).scrollY(.001).scrollX(-.001))
+  .layer(src(s0).scale(1, -1).scale(()=> 0.4 + a.fft[0]*0.01).repeat(1,1))
+  .contrast(3)
+  .color(.7,.9,.6)
+  // .invert(.1)
+  .contrast(2.5)
+  .scale(1)
+  // .kaleid(3)
+  // .scrollX(.1, .01)
+  // .modulatePixelate(src(o0), 10, 10)
+  .out(o0)
+
+
+s0.initImage('http://localhost:8080/pic.jpeg')
+src(o0)
+.layer(
+  src(s0)
+    .scale(() => 0.8 + a.fft[0]* 0.2)
+    .luma(.2)
+    .invert()
+    .kaleid([3, 5, 7])
+    .rotate(.5, .3)
+    .contrast(1.5)
+    .scrollX(.1, -.01)
+    .scrollY(.3)
+  )
+  .modulatePixelate(src(o0), [250, 500, 1000])
 .out(o0)
 
-
-
-
-voronoi(350,0.15)
-  	.modulateScale(osc(8).rotate(Math.sin(time)),.5)
-  	.thresh(() => a.fft[3]*0.01)
-    .scale(() => a.fft[2]*20)
-	.modulateRotate(osc(7), () => a.fft[1]*100)
-	.thresh(.7)
-  	.diff(src(o0).scale(() => a.fft[2]*10))
-	.modulateScale(osc(2).modulateRotate(o0,.74))
-	.diff(src(o0).rotate([-.012,.01,-.002,0]).scrollY(0,[-1/199800,2].fast(0.7)))
-	.brightness([-.02,-.17].smooth().fast(.25))
-	.out()
-
-
-
-
-  voronoi(8,1)
-  .mult(osc(10,0.1,()=>Math.sin(time)*3).saturate(3).kaleid(200))
-  .modulate(o0,a.fft[1]*2)
-  .add(o0,0.8)
-  .scrollY(-0.01)
-  .scale(()=> a.fft[1]*3)
-  .modulate(voronoi(8,1),a.fft[1]*0.001)
-  .luma(() => a.fft[0]*0.7)
-  .out()
-
-
-
-
-  osc(10, 0.9, 300)
-  .color(0.9, 0.7, 0.8)
-  .diff(
-    osc(45, 0.3, 100)
-    .color(0.9, 0.9, 0.9)
-    .rotate(0.18)
-    .pixelate(12)
-    .kaleid()
-  )
-  .scrollX(10)
-  .colorama()
-  .luma()
-  .repeatX(4)
-  .repeatY(4)
-  .modulate(
-    osc(1, -0.9, 300)
-  )
-  .scale(() => 3 + (a.fft[1]*1.1))
-  .out()
-
+hush()
 
 a.hide()
